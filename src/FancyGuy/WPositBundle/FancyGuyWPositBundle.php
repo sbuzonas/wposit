eebe3a060b1bc437ce88102b64fc7f1fa7f2cf00
Steve Buzonas <steve@fancyguy.com>
2014-01-01 16:32:43 -0500

    initial commit

---
+++ src/FancyGuy/WPositBundle/FancyGuyWPositBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FancyGuy\WPositBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FancyGuyWPositBundle extends Bundle
+{
+}
---
+++ src/FancyGuy/WPositWebBundle/FancyGuyWPositWebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FancyGuy\WPositWebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FancyGuyWPositWebBundle extends Bundle
+{
+}
